3ccbb3d8ef1f0026c6c5da7cf5402da389ac7789
cfilip04 <cfilip04@cs.ucy.ac.cy>
2013-11-13 00:42:37 +0200

    Prototype-version1.1
    -menu list change into tabs
    -check for invalid inputs
    -more user friendly environment

---
+++ SCC v1.1/stopCancerCyprus/src/com/example/stopcancercyprus/Settings.java
@@ -0,0 +1,50 @@
+package com.example.stopcancercyprus;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
+import android.widget.TextView;
+import android.widget.ToggleButton;
+
+public class Settings extends Activity{
+	ToggleButton checkNotificationActivity;
+	TextView displayNotificationActivity;
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		// TODO Auto-generated method stub
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.settings);
+		
+		createAllObjects();
+		
+		checkNotificationActivity.setOnClickListener(new View.OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				// TODO Auto-generated method stub
+				displayNotificationActivity.setText("Ειδοποίηση(Ανενεργή)");
+			}
+		});
+	}
+	
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+    
+    
+	private void createAllObjects() {
+		// TODO Auto-generated method stub
+		checkNotificationActivity= (ToggleButton) findViewById(R.id.deActivateNotification);
+		displayNotificationActivity=(TextView) findViewById(R.id.MsgPushNotification);
+	}
+	
+}
