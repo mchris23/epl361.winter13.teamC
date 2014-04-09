cf10148de15a2f15e5c06eaf15c24c5beb2cbb51
aneofy <aneofy04@cs.ucy.ac.cy>
2014-04-09 17:58:45 +0300

    Menu

--- 1st iteration/scc-1st iteration/Intro/src/cy/ac/ucy/teamc/scc/Menu.java
+++ 1st iteration/scc-1st iteration/Intro/src/cy/ac/ucy/teamc/scc/Menu.java
@@ -19,26 +19,33 @@ public class Menu extends TabActivity {
 		setContentView(R.layout.main);
 		menuTabHost=getTabHost();
 		
-		TabSpec generalSpec=menuTabHost.newTabSpec("Γενική Ενημέρωση");
-		generalSpec.setIndicator("Γενική Ενημέρωση",getResources().getDrawable(R.drawable.mytab1));
+		TabSpec generalSpec=menuTabHost.newTabSpec("ÃƒÃ¥Ã­Ã©ÃªÃž\nÃ…Ã­Ã§Ã¬ÃÃ±Ã¹Ã³Ã§");
+		generalSpec.setIndicator("ÃƒÃ¥Ã­Ã©ÃªÃž Ã…Ã­Ã§Ã¬ÃÃ±Ã¹Ã³Ã§",getResources().getDrawable(R.drawable.mytab1));
 		Intent generalIntent=new Intent(this,GeneralInform.class);
 		generalSpec.setContent(generalIntent);
 		
-		TabSpec personalSpec=menuTabHost.newTabSpec("Προσωπική Ενημέρωση");
-		personalSpec.setIndicator("Προσωπική Ενημέρωση",getResources().getDrawable(R.drawable.mytab2));
+		TabSpec personalSpec=menuTabHost.newTabSpec("ÃÃ±Ã¯Ã³Ã¹Ã°Ã©ÃªÃž\nÃ…Ã­Ã§Ã¬ÃÃ±Ã¹Ã³Ã§");
+		personalSpec.setIndicator("ÃÃ±Ã¯Ã³Ã¹Ã°Ã©ÃªÃž Ã…Ã­Ã§Ã¬ÃÃ±Ã¹Ã³Ã§",getResources().getDrawable(R.drawable.mytab2));
 		Intent personallIntent=new Intent(this,PersonalInform.class);
 		personalSpec.setContent(personallIntent);
 		
-		TabSpec settingsSpec=menuTabHost.newTabSpec("Ρυθμίσεις");
-		settingsSpec.setIndicator("Ρυθμίσεις",getResources().getDrawable(R.drawable.mytab3));
+		TabSpec settingsSpec=menuTabHost.newTabSpec("Ã‘ÃµÃ¨Ã¬ÃŸÃ³Ã¥Ã©Ã²");
+		settingsSpec.setIndicator("Ã‘ÃµÃ¨Ã¬ÃžÃ³Ã¥Ã©Ã²",getResources().getDrawable(R.drawable.mytab3));
 		Intent settingslIntent=new Intent(this,Settings.class);
 		settingsSpec.setContent(settingslIntent);
-			
+		
+		TabSpec pasikafinfo =menuTabHost.newTabSpec("Î Î›Î—Î¡ÎŸÎ¦ÎŸÎ¡Î™Î•Î£ Î Î‘Î£Î¥ÎšÎ‘Î¦");
+		pasikafinfo.setIndicator("Î Î›Î—Î¡ÎŸÎ¦ÎŸÎ¡Î™Î•Î£ Î Î‘Î£Î¥ÎšÎ‘Î¦",getResources().getDrawable(R.drawable.pasikaf));
+		Intent pasikafinfolIntent=new Intent("cy.ac.ucy.teamc.scc.PASIKAFINFO");
+		pasikafinfo.setContent(pasikafinfolIntent);
+		
 		menuTabHost.addTab(generalSpec);
 		menuTabHost.addTab(personalSpec);
 		menuTabHost.addTab(settingsSpec);
+		menuTabHost.addTab(pasikafinfo);
 
 	}
 
 
 }
+
